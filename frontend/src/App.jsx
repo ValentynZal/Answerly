13e32ab5a5aed3ea1978bf1d6439cfa79ae2e8d3
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2019-07-20 11:48:18 +0300

    add header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Header from './Components/Header';
+
+import createRoutes from './routes';
+
+const routes = createRoutes();
 
 function App() {
   return (
-    <div className="App">
-      Main page
-    </div>
+    <Router>
+      <Header />
+      { routes }
+    </Router>
   );
 }
 
